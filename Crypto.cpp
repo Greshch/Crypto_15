c58bb78e3df5b0577ba2a6b2947c9593d785f689
Greshch <greshchook@gmail.com>
2022-07-25 23:03:27 +0300

    prevKey = 0

--- Crypto.cpp
+++ Crypto.cpp
@@ -3,15 +3,25 @@
 
 void Encrypt(char* str, size_t szStr, char const* key, size_t szKey)
 {
+	char prevKey = 0;
 	for (size_t i = 0; i < szStr; i++)
 	{
 		int j = i % szKey;
-		char curentEncrypt = str[i] ^ key[j];
+		char curentEncrypt = str[i] ^ key[j] ^ prevKey;
+		prevKey = curentEncrypt;
 		str[i] = curentEncrypt;
 	}
 }
 
 void Decrypt(char* str, size_t szStr, char const* key, size_t szKey)
 {
-	Encrypt(str, szStr, key, szKey);
+	//Encrypt(str, szStr, key, szKey);
+	char prevKey = 0;
+	for (size_t i = 0; i < szStr; i++)
+	{
+		int j = i % szKey;
+		char curentDecrypt = str[i] ^ key[j] ^ prevKey;
+		prevKey = str[i];
+		str[i] = curentDecrypt;
+	}
 }
